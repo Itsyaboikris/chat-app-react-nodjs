84cb338fd910f71a34fc28c2f9215c713e4a9dc6
itsyaboikris <kristoff_riley@outlook.com>
2022-11-29 11:45:58 -0400

    contact listing

--- public/src/pages/Chat.js
+++ public/src/pages/Chat.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoutes";
+import { Contact } from "../components/Contact";
+import Welcome from "../components/Welcome";
 
 export default function Chat() {
+
+	const navigate = useNavigate();
+
+	const [contacts, setContacts] = useState([]);
+	const [currentUser, setCurrentUser] = useState(undefined);
+	const [currentChat, setCurrentChat] = useState(undefined);
+
+	useEffect(() => {
+
+		const checkForUser = async() => {
+			if(!localStorage.getItem('chat-app-user')) {
+				navigate('/login')
+			} else {
+				setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
+			}
+		}
+
+		checkForUser();		
+		
+	}, [])
+
+	useEffect(() => {
+		const getContacts = async () => {
+			
+			if (currentUser) {
+				if (currentUser.isAvatarImageSet) {
+				  	const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+				  	setContacts(data.data);
+				} else {
+				  	navigate("/setAvatar");
+				}
+			}
+			
+		}
+	
+		getContacts();
+	}, [currentUser])
+
+	const handleChatChange = (chat) => {
+		setCurrentChat(chat);
+	}
+
 	return(
 		<>
 			<Container>
 				<div className="container">
-
+					<Contact contacts ={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+					<Welcome currentUser={currentUser} currentUser={currentUser}/>
 				</div>
 			</Container>
 		</>	
@@ -29,7 +77,7 @@ const Container = styled.div`
 		background-color: #00000076;
 		display: grid;
 		grid-template-columns: 25% 75%;
-		
+
 		@media screen and (min-width: 720px) and (max-width: 1080px) {
 			grid-template-columns: 35% 65%;
 		}
